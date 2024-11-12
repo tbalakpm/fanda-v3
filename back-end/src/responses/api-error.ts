3aeba207fe7098192700d449e0a1eba9f3eeba12
Balamurugan Thanikachalam <software.balu@gmail.com>
2024-11-12 11:15:25 +0530

    Format task added

--- back-end/src/responses/api-error.ts
+++ back-end/src/responses/api-error.ts
@@ -1,5 +1,8 @@
 export class ApiError extends Error {
-  constructor(public override message: string, public statusCode: number) {
+  constructor(
+    public override message: string,
+    public statusCode: number
+  ) {
     super(message);
     this.name = this.constructor.name;
   }
