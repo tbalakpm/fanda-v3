ff6dce4371e7b549d8f044d1712f146da8c2ac70
Balamurugan Thanikachalam <software.balu@gmail.com>
2024-11-15 11:30:54 +0530

    Serialnumber for range

--- back-end/src/helpers/serial-number.helper.ts
+++ back-end/src/helpers/serial-number.helper.ts
@@ -5,60 +5,53 @@ export class SerialNumberHelper {
   private static readonly serialRepository = AppDataSource.getRepository(SerialNumber);
 
   static async getNextSerial(key: string, yearId: string): Promise<string> {
-    // const result = await this.serialRepository.update({ yearId, key }, { current: () => "current + 1" });
     const result = await this.serialRepository
       .createQueryBuilder()
       .update(SerialNumber)
       .set({ current: () => "current + 1" })
       .where("yearId = :yearId", { yearId })
       .andWhere("key = :key", { key })
-      .returning("*")
+      .returning("length, current, prefix")
       .execute();
     const serial = result.raw[0];
+    // console.log("serial", serial);
 
     if (!serial) {
       return "";
     }
 
-    const nextSerial = this.formatSerial(serial.length, serial.current - 1, serial.prefix);
-    return nextSerial;
+    const endSerial = this.formatSerial(serial.length, serial.current - 1, serial.prefix);
+    return endSerial;
   }
 
-  /*
-  static async getNextRangeSerial(count: number, key: string, yearId: string | null | undefined) {
-    const organization = await OrganizationModel.findOneAndUpdate(
-      {
-        "years._id": yearId,
-        "years.sequences.key": key
-      },
-      {
-        $inc: { "years.$[year].sequences.$[sequence].current": count }
-      },
-      {
-        arrayFilters: [{ "year._id": yearId }, { "sequence.key": key }],
-        session,
-        new: false,
-        upsert: true
-      }
-    );
+  static async getNextRangeSerial(
+    count: number,
+    key: string,
+    yearId?: string
+  ): Promise<{ beginSerial: string; endSerial: string; serial: { current?: number; length?: number; prefix?: string } }> {
+    const result = await this.serialRepository
+      .createQueryBuilder()
+      .update(SerialNumber)
+      .set({ current: () => `current + ${count}` })
+      .where("yearId = :yearId", { yearId })
+      .andWhere("key = :key", { key })
+      .returning("length, current, prefix")
+      .execute();
+    const serial = result.raw[0];
+    // console.log("serial", serial);
 
-    if (!organization) {
-      return { currentSerial: "", nextSerial: "", sequence: {} };
+    if (!serial) {
+      return { beginSerial: "", endSerial: "", serial: {} };
     }
 
-    const sequence = organization.years.id(yearId)?.sequences.find((s) => s.key === key);
-    if (!sequence) {
-      return { currentSerial: "", nextSerial: "", sequence: {} };
-    }
-    const currentSerial = this.formatSerial(sequence.length, sequence.current, sequence.prefix);
-    const nextSerial = this.formatSerial(sequence.length, sequence.current + (count - 1), sequence.prefix);
+    const beginSerial = this.formatSerial(serial.length, serial.current - count, serial.prefix);
+    const endSerial = this.formatSerial(serial.length, serial.current - count + (count - 1), serial.prefix);
     return {
-      currentSerial,
-      nextSerial,
-      sequence: { current: sequence.current, length: sequence.length, prefix: sequence.prefix }
+      beginSerial,
+      endSerial,
+      serial: { current: serial.current - count, length: serial.length, prefix: serial.prefix }
     };
   }
-  */
 
   static formatSerial(length: number, value: number, prefix?: string): string {
     return `${prefix ? prefix : ""}${String(value).padStart(length, "0")}`;
--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -4,7 +4,7 @@ import { AppDataSource } from "./data-source";
 import app from "./app";
 import logger from "./logger";
 import { createAdminUser } from "./data-seed/admin-user.data-seed";
-import { SerialNumberHelper } from "./helpers/serial-number.helper";
+// import { SerialNumberHelper } from "./helpers/serial-number.helper";
 
 const { PORT = "4000" } = process.env;
 
@@ -14,8 +14,11 @@ AppDataSource.initialize()
 
     await createAdminUser(db);
 
-    const current_serial = await SerialNumberHelper.getNextSerial("stock", "01932b09-e344-7998-9a52-06521371f6ad");
-    console.log("current_serial", current_serial);
+    // const current_serial = await SerialNumberHelper.getNextSerial("stock", "01932b09-e344-7998-9a52-06521371f6ad");
+    // console.log("current_serial", current_serial);
+
+    // const next_serial = await SerialNumberHelper.getNextRangeSerial(5, "stock", "01932b09-e344-7998-9a52-06521371f6ad");
+    // console.log("next_serial", next_serial);
 
     app.listen(PORT, () => {
       logger.info(`Server is running on http://localhost:${PORT}`);
