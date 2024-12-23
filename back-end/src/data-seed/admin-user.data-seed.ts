cac0ec84e1372295fa30ace5441517dce1f4e5d4
Balamurugan Thanikachalam <software.balu@gmail.com>
2024-12-23 11:23:06 +0530

    Removed default admin user creds from code

--- back-end/src/data-seed/admin-user.data-seed.ts
+++ back-end/src/data-seed/admin-user.data-seed.ts
@@ -4,16 +4,16 @@ import { encrypt } from '../helpers';
 import { UserRoles } from '../entities/role.enum';
 import 'dotenv/config';
 
-const { ADMIN_ID = '0192fc5e-0093-7ccc-a20a-dc844a5bad1c', ADMIN_USERNAME = 'admin', ADMIN_PASSWORD = 'Welcome!23' } = process.env;
+const { ADMIN_ID, ADMIN_USERNAME, ADMIN_PASSWORD } = process.env;
 
 export const createAdminUser = async (ds: DataSource) => {
   const userRepo = ds.getRepository<User>(User);
   const adminUserFound = await userRepo.existsBy({ username: 'admin' });
   if (!adminUserFound) {
     const adminUser = new User();
-    adminUser.userId = ADMIN_ID;
-    adminUser.username = ADMIN_USERNAME;
-    adminUser.password = await encrypt.encryptPassword(ADMIN_PASSWORD);
+    adminUser.userId = ADMIN_ID!;
+    adminUser.username = ADMIN_USERNAME!;
+    adminUser.password = await encrypt.encryptPassword(ADMIN_PASSWORD!);
     adminUser.role = UserRoles.Admin;
     await userRepo.save(adminUser);
   }
